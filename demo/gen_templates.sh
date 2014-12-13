46212c819ab7c9ae6ada29b9df9cdd8d1d39faea
Muayyad Alsadi <alsadi@gmail.com>
2014-12-13 14:47:56 +0200

    add arabic data

--- demo/gen_templates.sh
+++ demo/gen_templates.sh
@@ -4,3 +4,7 @@ cd `dirname $0`
 # to make this work type npm install -g handlebars@1.3.0
 ( cd handlebars/ && handlebars *.handlebars -m -f ../js/templates.js )
 
+( cd handlebars-ar/ && handlebars *.handlebars -m -f ../js/templates-ar.js )
+
+
+
